3606039e28ecef9176cc6dffd1cf66bc024cb7b3
tttai030306-prog <tttai030306@gmail.com>
2026-09-16 17:44:36 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -22,7 +22,7 @@ function login(username, password) {
   }
 
   // Kiểm tra tài khoản và mật khẩu
-  if (username === "admin" && password === "123") {
+  if (username === "admin" && password === "9999") {
     return true;
   }
 
